69386d4a1f2a3af3ba4e381886b77a3b7d44eab4
UXCam <uxcam@ip-192-168-1-65.ec2.internal>
2021-04-29 12:15:27 +0545

    Issue model and seeder added

---
+++ database/seeders/IssueSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Issue;
+
+class IssueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Issue::create([
+            'name' => 'Test'
+        ]);
+    }
+}
