fafb6d06a683e86d75efd45689265d4bf55f9922
UXCam <uxcam@ip-192-168-1-65.ec2.internal>
2021-04-29 16:22:46 +0545

    iot notifcation service added, wip

---
+++ app/Http/Services/NotificationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Issue;
+
+class NotificationService 
+{
+    public const IOT_ALERT = 'ring';
+    public const IOT_Duration = 1000; //misecond
+    public const IOT_DEVICE_ENDPOINT = 'https://m4adtdev41.execute-api.us-east-1.amazonaws.com/dev/alert';
+
+    public function notifyToIOTDevice($duration = NotificationService::IOT_Duration)
+    {
+    
+
+        // https://m4adtdev41.execute-api.us-east-1.amazonaws.com/dev/alert?alert=ring&duration=1000
+    }
+}
